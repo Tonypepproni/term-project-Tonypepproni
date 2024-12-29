d463d18bccea5601be73ee030e3ddb3ca8e04556
Nic0ro <willarossi1@gmail.com>
2024-12-28 22:18:00 -0500

    national parks page

--- main.py
+++ main.py
@@ -24,14 +24,31 @@ class Basic(View):
             nav_items=self.nav_items,
             name=self.name
         )
+    
+class park(Basic):
+    def __init__(self, name, template_name, park):
+        super().__init__(name, template_name)
+        self.park = park
+
+    def dispatch_request(self):
+        return render_template(
+            self.template_name,
+            nav_items=self.nav_items,
+            name=self.name,
+            parks=self.park            
+        )
 
 with open("static/info/parks.json") as parkInfo:
     file_contents= json.load(parkInfo)
 
-print(file_contents)
+natParks = file_contents["np"]
+
+print(natParks)
+
+print(((natParks['Acadia'])["PreImg"])["img"])
 
 main.add_url_rule('/', view_func = Basic.as_view('home','home','home.html'))
-main.add_url_rule('/nationalPark', view_func = Basic.as_view('NationalPark','NationalPark','nationalPark.html'))
+main.add_url_rule('/natPark', view_func = park.as_view('NationalPark','NationalPark','nationalPark.html',natParks))
 main.add_url_rule('/swap', view_func = Basic.as_view('Swap','Swap','swap.html'))
 
 if __name__ =='__main__':
